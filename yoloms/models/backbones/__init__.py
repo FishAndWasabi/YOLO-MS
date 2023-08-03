149a9b848db1022c50dff3c16101cbc7f0b0acf9
root <chenyuming@mail.nankai.edu.cn>
2023-08-04 00:16:57 +0800

    [Update] yolomsv6

--- yoloms/models/backbones/__init__.py
+++ yoloms/models/backbones/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .yoloms import YOLOMS
 from .yolomsv8 import YOLOMSv8
+from .yolomsv6 import YOLOMSv6
 
 __all__ = [
-    'YOLOMS', 'YOLOMSv8'
+    'YOLOMS', 'YOLOMSv8', 'YOLOMSv6'
 ]
