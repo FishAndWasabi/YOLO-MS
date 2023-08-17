8e102811c7d4180a8fd7a4eec80bd830ecf48cc8
cym <chenyuming@mail.nankai.edu.cn>
2023-08-17 13:38:38 +0800

    [Update]

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ conda create -y -n YOLO-MS python=3.8
 conda activate YOLO-MS
 
 # install pytorch
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 
 # install dependencies of openmmlab
 pip install -U openmim
