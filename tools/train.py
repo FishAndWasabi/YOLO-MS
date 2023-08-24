d3f7a45c19e5e371ac3b2ee500eae552b72e64c1
cym <chenyuming@mail.nankai.edu.cn>
2023-08-24 08:43:01 +0800

    [Update] fix bug

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,6 @@
 # Refer to MMYOLO
 # Copyright (c) MCG-NKU. All rights reserved.
+import argparse
 import logging
 import os
 import os.path as osp
@@ -7,21 +8,20 @@ import os.path as osp
 from mmengine.config import Config, DictAction
 from mmengine.logging import print_log
 from mmengine.runner import Runner
-
 from mmyolo.registry import RUNNERS
 from mmyolo.utils import is_metainfo_lower
 
 from yoloms import *
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
-    parser.add_argument(
-        '--amp',
-        action='store_true',
-        default=False,
-        help='enable automatic-mixed-precision training')
+    parser.add_argument('--amp',
+                        action='store_true',
+                        default=False,
+                        help='enable automatic-mixed-precision training')
     parser.add_argument(
         '--resume',
         nargs='?',
@@ -40,11 +40,10 @@ def parse_args():
         'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
         'Note that the quotation marks are necessary and that no white space '
         'is allowed.')
-    parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
+    parser.add_argument('--launcher',
+                        choices=['none', 'pytorch', 'slurm', 'mpi'],
+                        default='none',
+                        help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
@@ -77,10 +76,9 @@ def main():
     if args.amp is True:
         optim_wrapper = cfg.optim_wrapper.type
         if optim_wrapper == 'AmpOptimWrapper':
-            print_log(
-                'AMP training is already enabled in your config.',
-                logger='current',
-                level=logging.WARNING)
+            print_log('AMP training is already enabled in your config.',
+                      logger='current',
+                      level=logging.WARNING)
         else:
             assert optim_wrapper == 'OptimWrapper', (
                 '`--amp` is only supported when the optimizer wrapper type is '
